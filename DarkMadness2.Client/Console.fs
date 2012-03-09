102e99f117070191a7acb689e3ddbe8818452e5d
yurii-litvinov <yurii.litvinov@gmail.com>
2012-03-09 19:40:26 +0400

    Madness returns!

---
+++ DarkMadness2.Client/Console.fs
@@ -0,0 +1,27 @@
+﻿module DarkMadness2.Console
+
+open System
+
+// Window-related functions
+let setWindowSize w h = Console.SetWindowSize (w, h)
+let maximizeWindow () = 
+    setWindowSize Console.LargestWindowWidth Console.LargestWindowHeight
+    Console.SetBufferSize (Console.LargestWindowWidth, Console.LargestWindowHeight)
+
+// Cursor-related functions
+let setCursorPosition x y = Console.SetCursorPosition (x, y)
+let showCursor () = Console.CursorVisible <- true
+let hideCursor () = Console.CursorVisible <- false
+
+// Drawing functions
+let putCharWithColor ch foreground background (x, y) = 
+    setCursorPosition x y
+    Console.ForegroundColor <- foreground
+    Console.BackgroundColor <- background
+    Console.Write (ch : char)
+
+let putChar ch (x, y) =
+    setCursorPosition x y
+    Console.Write (ch : char)
+
+
---
+++ DarkMadness2.Client/Program.fs
@@ -0,0 +1,24 @@
+﻿open DarkMadness2.Console
+
+let mutable charPosition = (10, 10)
+
+let (+) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+let processKeyPress (key : System.ConsoleKeyInfo) =
+    putChar ' ' charPosition
+    let delta = match key.Key with
+                | System.ConsoleKey.UpArrow -> (0, -1)
+                | System.ConsoleKey.DownArrow -> (0, 1)
+                | System.ConsoleKey.LeftArrow -> (-1, 0)
+                | System.ConsoleKey.RightArrow -> (1, 0)
+                | _ -> (0, 0)
+    charPosition <- charPosition + delta
+    putChar '@' charPosition
+
+maximizeWindow ()
+hideCursor ()
+putChar '@' charPosition
+
+Seq.initInfinite (fun _ -> System.Console.ReadKey true) 
+    |> Seq.takeWhile (fun key -> key.Key <> System.ConsoleKey.Escape)
+    |> Seq.iter processKeyPress
